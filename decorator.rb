6a2b49c5ebe7fc40a469072af1029d48175ab936
kerlosbahaa3 <aboelking4@gmail.com>
2023-09-15 14:22:26 +0300

    Fix

--- decorator.rb
+++ decorator.rb
@@ -1,28 +1,14 @@
-# Base Decorator class
-require_relative 'person'
+require_relative 'nameable'
 
 class Decorator < Nameable
   attr_accessor :nameable
 
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
 
   def correct_name
     @nameable.correct_name
-  end
-end
-
-# CapitalizeDecorator class
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-# TrimmerDecorator class
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name[0..9]
   end
 end
