173e9ac687a520be3064595f89ebce11a205e5a6
kerlos bahaa <123717878+kerlos-bahaa@users.noreply.github.com>
2023-09-15 14:05:48 +0300

    Update decorator.rb
    Co-authored-by: Aamir khan <aamir.khan2478@gmail.com>

--- decorator.rb
+++ decorator.rb
@@ -2,10 +2,16 @@
 require_relative 'person'
 
 class Decorator < Nameable
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
+    attr_accessor :nameable
+  
+    def initialize(nameable)
+      @nameable = nameable
+    end
+  
+
+    def correct_name
+      @nameable.correct_name
+    end
 end
 
 # CapitalizeDecorator class
