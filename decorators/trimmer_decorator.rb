c944bc8b75f552bf6a909311f42292045bf221c7
kerlosbahaa3 <aboelking4@gmail.com>
2023-09-18 11:48:12 +0300

    refactor

--- decorators/trimmer_decorator.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'decorator'
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    original_name = @nameable.correct_name
-    original_name.length > 10 ? original_name[0..9] : original_name
-  end
-end
